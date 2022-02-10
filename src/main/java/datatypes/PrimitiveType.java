3160f3b21ef92eb939d5231a144aa2c1aaddd454
sujana <sujanainavolu@gmail.com>
2022-02-10 10:25:13 +0530

    [Datatypes1] Primitivedatatypes

---
+++ src/main/java/datatypes/PrimitiveType.java
@@ -0,0 +1,23 @@
+package datatypes;
+
+public class PrimitiveType {
+	public static void main(String[] args) {
+		int i = 5;
+		char a = 'G';
+		byte b = 4;
+		short s = 56;
+		long l = -423320L;
+		double d = 4.35;
+		float f = 4.73f;
+		boolean B = true;
+		System.out.println("boolean: " + B);
+		System.out.println("char: " + a);
+		System.out.println("integer: " + i);
+		System.out.println("byte: " + b);
+		System.out.println("short: " + s);
+		System.out.println("long: " + l);
+		System.out.println("float: " + f);
+		System.out.println("double: " + d);
+
+	}
+}
